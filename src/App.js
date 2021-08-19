3dece97627679a0531682ee6f07abe3e92086fe7
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-08-19 17:39:50 +0200

    Form Exo 1>2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reg= /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      rememberMe: false,
+      emailIsValid: false,
+      passwordIsValid: false,
+      isSubmitted: false,
+    };
+  }
+
+  
+  //Saisie d'adresse mail
+
+  handleEmailChange = (e) => {
+    this.setState({ email: e.target.value, 
+                    emailIsValid: reg.test(e.target.value)})
+    console.log(this.state);
+    
+  };
+
+  //Saisie Mot de passe
+
+  handlePasswordChange = (e) => {
+    // console.log(e);
+    this.setState({ password: e.target.value,
+                    passwordIsValid: (e.target.value.length>5) });
+    console.log(this.state);
+  };
+
+  //Se rappler de User ID
+  handleRememberMeChange = (e) => {
+    console.log(e);
+    this.setState({ rememberMe: e.target.value });
+  };
+
+  //Validation ID "True or False"
+  handleSubmit = (e) => {
+
+  };
+
+  render() {
+    return (
+      <>
+        <p> Sofiane.ab@gmail.com... {this.state.email}</p>
+        <form onSubmit={this.onFormSubmit}>
+          <input 
+            type="email" 
+            onChange={this.handleEmailChange}
+            value= {this.state.email} />
+
+          <input
+            type="password"
+            onChange={this.handlePasswordChange}
+            value= {this.state.password}
+          />
+          
+           <button 
+            type="submit" 
+            onChange={this.handlePasswordChange}
+            value= {this.state.password}>
+            Validation email adress 
+            
+          </button>
+
+          
+        </form>
+      </>
+    );
+  }
 }
 
 export default App;
